9a38b4a03d7ee7875b995ca4f4554c977af4f5ed
KhaledKammoun <khaledsfax03@gmail.com>
2023-09-19 00:17:04 +0100

    Tic-Tac-Toe-with-AI

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import random
 def initializeBoard() :
     return [' ' for _ in range(9)]
 def printBoard() :
-    print("{} | {} | {} ".format(board[0],board[1],board[2]))
-    print("----------")
-    print("{} | {} | {} ".format(board[3],board[4],board[5]))
-    print("----------")
-    print("{} | {} | {} ".format(board[6],board[7],board[8]))
+    print("    {} | {} | {}   |   1 | 2 | 3".format(board[0],board[1],board[2]))
+    print("   -----------  |  -----------")
+    print("    {} | {} | {}   |   4 | 5 | 6".format(board[3],board[4],board[5]))
+    print("   -----------  |  -----------")
+    print("    {} | {} | {}   |   7 | 8 | 9  ".format(board[6],board[7],board[8]))
 def boardIsFull() :
     return board.count(' ')==0
 def caseIsempty(pos) :
@@ -14,23 +14,24 @@ def caseIsempty(pos) :
 def PlayerMove() :
     test = True 
     while test :
-        pos = input("Enter a number (1-9) : ")
+        pos = input("\nEnter a number (1-9) : ")
         try :
             pos = int(pos)
-            if 0 > pos <= 9 :
+            if 0 < pos <= 9 :
                 if caseIsempty(pos) :
                     board[pos-1] = 'X'
                     test = False
                 else :
-                    print("Error, this case is not empty, try an other number!")
+                    print("\nError, this case is not empty, try an other number!\n")
             else :
-                print("Error, Give a number within the range!")
+                print("\nError, Give a number within the range!\n")
         except :
-            print("Error, you shoud write a number!")
+            print("\nError, you shoud write a number!\n")
 def compMove() :
     notFullCases = [index for index in range(9) if caseIsempty(index)]
     pos = random.choice(notFullCases)
     board[pos] = 'O'
+    print("\nThe player O fill the {}th case\n".format(pos+1))
 
 def winner() :
     for i in range(3) :
@@ -42,21 +43,33 @@ def winner() :
     return False
 def main() :
     global board
-    board = initializeBoard()
-    printBoard()
-    x = 0
-    while not(boardIsFull()) :
-        if x%2 == 0 : # O is playing
-            compMove()
-            if winner() :
-                print("Sorry, O/'s the winner, For the next time :(")
-                break
-        else :
-            PlayerMove()
-            if winner() :
-                print("Congratulation!!, You win!!!!")
-                break
-        x+=1
+    playAgain = True
+    while playAgain :
+        board = initializeBoard()
+        print("-----------------")
+        print("|  Tic Tac Toe  |")
+        print("-----------------")
         printBoard()
+        print("--------------------")
+        print("|***Let We Start***|")
+        print("--------------------")
+        x = 0
+    
+        while not(boardIsFull()) :
+            if x%2 == 0 : # O is playing
+                compMove()
+                if winner() :
+                    print("Sorry, O/'s the winner, For the next time :(")
+                    break
+            else :
+                PlayerMove()
+                if winner() :
+                    print("Congratulation!!, You win!!!!")
+                    break
+            x+=1
+            print()
+            printBoard()
+        playAgain = input("\nYou wanna play again!(Y/N) : ") == "Y"
+
 
 main()
