b8fe5e5220278ffa268ea017c4826ff489bc8d29
KhaledKammoun <khaledsfax03@gmail.com>
2023-10-14 00:24:16 +0100

    Tic-Tac-Toe-With-AI

--- design.py
+++ design.py
@@ -1,4 +1,54 @@
-import time
+import time, random
+def fillSpaces(string) :
+    listOfSpaces = []
+    i, j =0, len(string) - 1
+    while i<j :
+        if not string[i+2].isalpha() and string[i] == ' ' :
+            listOfSpaces.append(i)
+            string = string[:i] + '|' + string[i+1:]
+        i+=1
+        if not string[j-2].isalpha() and string[j] == ' ' :
+            listOfSpaces.append(j)
+            string = string[:j] + '|' + string[j+1:]
+        j-=1
+    return listOfSpaces
+
+# boards : contain all the used board in the game, as a matrix .
+boards = []
+def fillingBoardWithWord(x, y, word, boardTypeIndex) :
+    # call the board[boardTypeIndex] to get the board
+    # then, fill the specific case
+    # in any changes, call a print function with boardTypeIndex as a argument
+    pass
+
+# fill a random case by calling then the fillingBoardWithWord 
+def animatedFillingBoard(listOfSpaces) :
+    while listOfSpaces != [] :
+        randomIndex = random.choice(listOfSpaces)
+        
+        listOfSpaces.remove(randomIndex)
+
+# make a matrix with all the free spaces, and then call the animatedFillingBoard to make it general
+# to all the board cases, not just for a case in a ligne .
+fillSpaces("|||  | ||| |||  Tic Tac Toe  || || || |  ||")
+
+"""
+print("\033c", end='')
+print("        |||||||||||||||||||||||||||||||||||||||||||")
+print("        |||  | ||| |||  Tic Tac Toe  || || || |  ||")
+print("        |||||||||||||||||||||||||||||||||||||||||||")
+print("        ||||| ||        || ||| |         ||| |  |||")
+print("        ||             ||     ||  |    |      |||||")
+print("        ||               Welcome                 ||")
+print("        ||||||||||                       ||||||||||")
+print("        |||||||||                          ||||||||")
+print("        ||||||||||||||               ||||||||||||||")
+print("        ||                                       ||")
+print("        |||                                     |||")
+print("        |||||||  ||                        |  |||||")
+print("        |||  ||||||| |  |   ||  |||     ||||   ||||")
+print("        |||||||||||||||||||||||||||||||||||||||||||")
+time.sleep(0.7)
 print("\033c", end='')
 print("        |||||||||||||||||||||||||||||||||||||||||||")
 print("        |||  | ||| |||  Tic Tac Toe  || || || |  ||")
@@ -14,7 +64,7 @@ print("        |||                                     |||")
 print("        |||||||  ||                        |  |||||")
 print("        |||  ||||||| |  |   ||  |||     ||||   ||||")
 print("        |||||||||||||||||||||||||||||||||||||||||||")
-time.sleep(1.0)
+time.sleep(0.7)
 print("\033c", end='')
 print("        |||||||||||||||||||||||||||||||||||||||||||")
 print("        |||  | ||| |||  Tic Tac Toe  || || || |  ||")
@@ -30,7 +80,7 @@ print("        |||                                     |||")
 print("        |||||||  ||                        |  |||||")
 print("        |||  ||||||| |  |   ||  |||     ||||   ||||")
 print("        |||||||||||||||||||||||||||||||||||||||||||")
-time.sleep(2)
+time.sleep(0.7)
 print("\033c", end='')
 print("        |||||||||||||||||||||||||||||||||||||||||||")
 print("        |||  | ||| |||  Tic Tac Toe  || || || |  ||")
@@ -70,8 +120,9 @@ print("        |||||||| ")
 difficulty = int(input("        ||||||||||| KEY :: "))
 
 
-"""
+
 # board printing
+print("\033c", end='')
 print("        |||||||||||||||||||||||||||||||||||||||||||")
 print("        |||  | ||| |||  Tic Tac Toe  || || || |  ||")
 print("        |||||||||||||||||||||||||||||||||||||||||||")
@@ -86,16 +137,4 @@ print("        ||                                       ||")
 print("        |||||||  |||  ||| ||||  |  |||  |||  ||||||")
 print("        |||  ||||||| |||  || |||||  ||| ||||   ||||")
 print("        |||||||||||||||||||||||||||||||||||||||||||")
-
-
-print("  #   #  ||   ###   ||  #   #  ")     
-print("   ###   ||  #   #  ||   ###   ")
-print("  #   #  ||   ###   ||  #   #  ")
-print("|||||||||||||||||||||||||||||||")
-print("         ||         ||         ")
-print("    X    ||    X    ||    X    ")
-print("         ||         ||         ")
-print("|||||||||||||||||||||||||||||||")
-print("         ||         ||         ")
-print("    X    ||    X    ||    X    ")
-print("         ||         ||         ")"""
+"""
