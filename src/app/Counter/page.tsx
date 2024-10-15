514d49c4b3e988d9385ebf684b1c919ce2a1ca70
KhaledKammoun <khaledsfax03@gmail.com>
2024-10-15 12:51:14 +0100

    First Commit

--- src/app/Counter/page.tsx
+++ src/app/Counter/page.tsx
@@ -3,20 +3,17 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 interface CounterState {
-  count: number; // Define the type for counter
+  count: number;
 }
 
 function Counter() {
-  const { count }: CounterState = useSelector((state: any) => state.counter); // Adjusted type
+  const { count }: CounterState = useSelector((state: any) => state.counter);
   const dispatchCounter = useDispatch();
-  console.log(count);
-  return (
-    <div className="flex flex-col">
-      <div className="font-bold text-lg text-gray-700 dark:text-gray-600">
-        Counter: {count}
-      </div>
 
-      <div className="flex flex-row custom-button">
+  return (
+    <div>
+      <div>Counter: {count}</div>
+      <div>
         <div onClick={() => dispatchCounter({ type: "INC" })}>INCREMENT</div>
         <div onClick={() => dispatchCounter({ type: "DEC" })}>DECREMENT</div>
         <div onClick={() => dispatchCounter({ type: "RES" })}>RESET</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,14 @@
-// Home.tsx
+// src/app/page.tsx
 "use client";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import Square from "./Square/page"; // Assuming Game component is in a separate file
-import { Provider } from "react-redux";
-import Counter from "./Counter/page";
-import _Project from "./Project/page";
-import store from "./Context/store";
 
-import { NavLink } from "react-router-dom";
+import Counter from "./Counter/page";
+
 const Home = () => {
-  if (!store) {
-    return <div>Loading...</div>;
-  }
-  // const [isCounterOpen, setIsCounterOpen] = useState(false);
   return (
-    <Provider store={store}>
-      <div className="text-black dark:text-black flex flex-row justify-center items-center">
-        <_Project />
-      </div>
-    </Provider>
+    <div>
+      <h1>Home Page</h1>
+      <Counter />
+    </div>
   );
 };
 
